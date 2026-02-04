953553e8dce9e645efd18d97ae131878ce301263
CloviusIX <github@lorisleonard.com>
2026-02-04 22:35:36 +0100

    refactor: remove unnecessary async entrypoint

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import asyncio
-
-from aave_umbrella.forks.account import get_user_account
-from aave_umbrella.forks.funding import fund_user
-from aave_umbrella.providers.web3_client import build_web3_connection
-
-
-async def main() -> None:
-    web3 = await build_web3_connection()
-    user_account = await get_user_account(web3)
-    await fund_user(web3, user_account)
+def main() -> None:
+    print("Hello world !!!")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
